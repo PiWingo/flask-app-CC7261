5587a9ff83e49272d9233fdbe1818ca187d84361
Pietro <pietro.montoro@gmail.com>
2020-04-26 22:20:13 -0300

    added logic

--- deploy.py
+++ deploy.py
@@ -1,10 +1,54 @@
-from flask import Flask
+from flask import Flask, request
+import threading
+import time
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+volume = 0
+
+@app.route('/secador', methods=['POST'])
+def postVolume():
+    data = request.get_json()
+
+    global volume
+    volume += (data['solucao'] * .99)
+
+    response = {
+        'volume': volume
+    }
+
+    return response
+
+
+@app.route('/secador', methods=['GET'])
+def getVolume():
+    global volume
+    response = {
+        'biodiesel': volume
+    }
+    return response
+
+
+class Secador(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while True:
+            global volume
+            if volume > 0:
+                time.sleep(3)
+                request = {
+                    'biodiesel': volume
+                }
+                print('sending volume to storage tank')
+                print(request)
+                #fazer callout pro tanque de biodisel
+                volume = 0
+
+
 
 if __name__ == '__main__':
+    sec = Secador()
+    sec.start()
     app.run()
